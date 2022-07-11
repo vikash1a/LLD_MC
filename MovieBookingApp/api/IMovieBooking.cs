04cf31895b15767023bcaff0b9dcfb025183aa2b
vikash1a <vikash1a2b3c@gmail.com>
2022-07-11 23:48:41 +0530

    Movie Booking done, testing can be improved

--- MovieBookingApp/api/IMovieBooking.cs
+++ MovieBookingApp/api/IMovieBooking.cs
@@ -1,23 +1,14 @@
 
+using System.Collections.Generic;
+
 namespace api
 {
     public interface IMovieBooking
     {
-        public int CreateUser(User user);
-        public User GetUser(int userId);
-
-        public int CreateMovie(Movie movie);
-        public Movie GetMovie(int movieId);
-        public List<Movie> GetMovies(string searchText);
-
-        public int CreateTheater(Theater theater);
-        public Theater GetTheater(int theaterId);
-        public List<Movie> GetMoviesInTheater(int theaterId);
-
-        public int CreateShow(Show show);
-        public Show GetShow(int showId);
-
-        public int CreateTicket(Ticket ticket);
-        public Ticket GetTicket(int ticketId);
+        public List<string> listShows(string movieName);
+        public List<int> availableSeats(string showId);
+        public string createBooking(string userId, string showId, HashSet<int> bookedSeats);
+        public string updatePayment(string bookingId, bool paymentStatus);
+        public string GetUser(string name);
     }
 }
