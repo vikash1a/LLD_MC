f25b91395030260ed37cf88507b2844282e9aec9
vikash1a <vikash1a2b3c@gmail.com>
2022-08-26 20:46:32 +0530

    at work code added

---
+++ AtWork/MessageQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
